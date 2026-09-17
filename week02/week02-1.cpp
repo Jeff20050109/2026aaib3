517cd998fb51e8a6ce9f4259d331f65ac46e1fbd
Jeff20050109 <144758510+Jeff20050109@users.noreply.github.com>
2026-09-17 11:52:50 +0800

    week02
    week02

---
+++ week02/week02-1.cpp
@@ -0,0 +1,11 @@
+//week02-1 .cpp
+#include <iostream>
+#include <string>
+///囉嗦版
+int main()
+{
+    std::cout << "請問你叫什麼名字? ";
+    std::string name;///宣告字串
+    std::cin >> name;
+    std::cout << name << "你好, 今天教字串";
+}
---
+++ week02/week02-2.cpp
@@ -0,0 +1,11 @@
+///week02-2 .cpp
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    cout << "請問你叫什麼名字? ";
+    string name;///宣告字串
+    cin >> name;
+    cout << name << "你好, 今天教命名空間";
+}
